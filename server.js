1d3df145f646f2ef6ca9788416e01ebb758143cc
Jelil Adesina <jtutoff@gmail.com>
2023-03-28 06:08:48 +0100

    updated server.js and views

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 const port = 3001;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.use('/', routes());
+
+app.get('/', (request, response) => {
+  response.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.listen(port, () => {
