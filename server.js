be907cd1ead48b9fb6302e87689fc00806ca871d
Jelil Adesina <jtutoff@gmail.com>
2023-03-28 06:41:05 +0100

    added cookie-session

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
 const routes = require('./routes');
 
 const app = express();
 const port = 3001;
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['ksdjfcadsiew', 'owecdkhdsowesh'],
+  })
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
