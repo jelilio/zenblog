02205efc2820643dfa4f1d90cfba21e9aabecdec
Jelil Adesina <jtutoff@gmail.com>
2023-03-28 04:25:27 +0100

    removed unused comments

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
-// const routes = require('./routes');
 
 const app = express();
 const port = 3001;
 
-// app.use('/', routes());
-
-app.use(express.static(path.join(__dirname, './static'))); // middleware
+app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
