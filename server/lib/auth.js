25aca36c85d7c4e592f4dd6ad8beca766802a131
Jelil Adesina <jtutoff@gmail.com>
2023-03-31 01:14:09 +0100

    user serializer and deserializer implemented

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -22,6 +22,18 @@ passport.use(
   })
 );
 
+// eslint-disable-next-line no-underscore-dangle
+passport.serializeUser((user, done) => done(null, user._id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await UserModel.findById(id).exec();
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
+
 module.exports = {
   initialize: passport.initialize(),
   session: passport.session(),
