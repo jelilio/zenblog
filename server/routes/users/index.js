e0f84920c3f11a1cc5dd31caac72a742dbe38385
Jelil Adesina <jtutoff@gmail.com>
2023-03-31 12:16:41 +0100

    refactored register route

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -3,11 +3,20 @@ const passport = require('passport');
 
 const router = express.Router();
 
+const renderRegister = (req, res, next, success, errMessage) =>
+  res.render('layout', {
+    pageTitle: 'Register',
+    template: 'users/register',
+    errMessage,
+    success,
+    formData: req.body,
+  });
+
 module.exports = ({ userService }) => {
   router.get('/login', (request, response) => {
     response.render('layout', {
       pageTitle: 'Login',
-      template: 'login',
+      template: 'users/login',
       error: request.query.error,
       formData: {},
     });
@@ -21,13 +30,8 @@ module.exports = ({ userService }) => {
     })
   );
 
-  router.get('/register', (request, response) => {
-    response.render('layout', {
-      pageTitle: 'Register',
-      template: 'register',
-      success: request.query.success,
-      formData: {},
-    });
+  router.get('/register', (request, response, next) => {
+    renderRegister(request, response, next, request.query.success);
   });
 
   router.post('/register', async (req, res, next) => {
@@ -39,15 +43,9 @@ module.exports = ({ userService }) => {
       );
       if (savedUser) return res.redirect('/users/register?success=true');
 
-      return next(new Error('Failed to saved used'));
+      return renderRegister(req, res, next, false, 'Failed to saved user');
     } catch (err) {
-      return res.render('layout', {
-        pageTitle: 'Register',
-        template: 'register',
-        errMessage: err.message,
-        success: false,
-        formData: req.body,
-      });
+      return renderRegister(req, res, next, false, err.message);
     }
   });
 
