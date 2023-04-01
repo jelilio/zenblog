747f49f7bb6332dc85c2fc25a6f9238630431076
Jelil Adesina <jtutoff@gmail.com>
2023-04-01 02:50:27 +0100

    updated gitignore

--- README.md
+++
@@ -1,3 +0,0 @@
-# Building a Blog using Node.js and Express
-
-This repo is a demo project on building a blog using Nodejs and ExpressJs
--- server/lib/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
-// Included because it removes preparatory warnings for Mongoose 7.
-// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
-mongoose.set('strictQuery', false);
-
-module.exports.connect = async (dsn) => mongoose.connect(dsn, { useNewUrlparser: true });
